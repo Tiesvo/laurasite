25b8f4769089c833d7bb083dba37dbd7da565c95
Ties <ties@vanoldenborgh.com>
2026-02-24 15:16:40 +0400

    Initial commit

--- app/_data/content.ts
+++ app/_data/content.ts
@@ -3,7 +3,7 @@ export const content = {
   title: "Model & Creator",
   tagline: "You’re favorite Belgian girl 😊",
   ctas: [
-    { label: "Onlyfans", href: "https://onlyfans.com/lauraclauw", brand: "onlyfans" },
+    { label: "Onlyfans", href: "https://onlyfans.com/lauraclauw/c1", brand: "onlyfans" },
     { label: "F2F", href: "https://f2f.com/s/srKpPh", brand: "f2f" },
   ],
   gallery: Array.from({ length: 19 }).map((_, i) =>
